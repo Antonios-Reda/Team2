875a1e11bffbff7c98c0fab7cf8d9ffb20abdf6d
Antonios <tonyreda63@gmail.com>
2026-04-18 14:58:29 +0200

    fix router functions import

--- Backend/users/user.router.js
+++ Backend/users/user.router.js
@@ -1,35 +1,45 @@
 const router = require("express").Router();
 const { checkToken } = require("../middleware/jwt_validation");
+
+const userController = require("./user.controller");
+
 const {
-    createUser,
-    login,
-    updateUsers,
-    deleteUser,
-    validateUser,
-    getDoctors,
-    addToWaitList,
-    getWaitingPatients,
-    removePatientFromWaitlist,
-    addPrescription,
-    getPrescription
-} = require("./user.controller");
+  createUser,
+  login,
+  updateUsers,
+  deleteUser,
+  validateUser,
+  getDoctors,
+  addToWaitList,
+  getWaitingPatients,
+  removePatientFromWaitlist,
+  addPrescription,
+  getPrescription,
+} = userController;
 
-
+// validate token
 router.get("/validate", checkToken, validateUser);
 
+// auth
 router.post("/login", login);
 router.post("/signup", createUser);
+
+// doctors
 router.post("/getdoctors", getDoctors);
+
+// waiting list
 router.post("/addtowaitlist", addToWaitList);
 router.post("/getwaitingpatients", getWaitingPatients);
 router.post("/removefromwaitlist", removePatientFromWaitlist);
+
+// prescriptions
 router.post("/addprescription", addPrescription);
 router.post("/getprescription", getPrescription);
 
-
-
+// update user
 router.patch("/", checkToken, updateUsers);
-router.delete("/doctor", checkToken, deleteUser);
 
+// delete doctor
+router.delete("/doctor", checkToken, deleteUser);
 
 module.exports = router;
